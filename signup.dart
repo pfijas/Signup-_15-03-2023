9f0137b89b8f5ed7583baf664b849c595cc3bf03
pfijas <101388438+pfijas@users.noreply.github.com>
2023-03-15 16:23:11 +0530

    Add files via upload

---
+++ signup.dart
@@ -0,0 +1,136 @@
+
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading:
+        IconButton( onPressed: (){
+          Navigator.pop(context);
+        },icon:Icon(Icons.arrow_back_ios,size: 20,color: Colors.black,)),
+      ),
+      body: SafeArea(
+        child: ListView(
+          children:[ Card(
+            elevation: 50,
+            margin: EdgeInsets.symmetric(vertical: 100,horizontal: 100),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text ("Sign up", style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                        SizedBox(height: 20,),
+                        Text("Create an Account,Its free",style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey[700],
+                        ),),
+                        SizedBox(height: 30,)
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 40
+                      ),
+                      child: Column(
+                        children: [
+                          makeInput(label: "Full name"),
+                          makeInput(label: "Email"),
+                          makeInput(label: "Password",obsureText: true),
+                          makeInput(label: "Confirm Pasword",obsureText: true)
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: EdgeInsets.only(top: 3,left: 3),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            border: Border(
+                                bottom: BorderSide(color: Colors.black),
+                                top: BorderSide(color: Colors.black),
+                                right: BorderSide(color: Colors.black),
+                                left: BorderSide(color: Colors.black)
+                            )
+                        ),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height:40,
+                          onPressed: (){},
+                          color: Colors.redAccent,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40)
+                          ),
+                          child: Text("Sign Up",style: TextStyle(
+                            fontWeight: FontWeight.w600,fontSize: 16,
+
+                          ),),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Already have an account? "),
+                        Text("Login",style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18
+                        ),),
+                      ],
+                    )
+                  ],
+
+                ),
+              ],
+            ),
+          ),
+      ]
+        ),
+      ),
+    );
+  }
+}
+
+Widget makeInput({label,obsureText = false}){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(label,style:TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.w400,
+          color: Colors.black87
+      ),),
+      SizedBox(height: 5,),
+      TextField(
+        obscureText: obsureText,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white,
+            ),
+          ),
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white)
+          ),
+        ),
+      ),
+      SizedBox(height: 30,)
+
+    ],
+  );
+}
